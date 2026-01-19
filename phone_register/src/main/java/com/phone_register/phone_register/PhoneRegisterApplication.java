76ca3e12afe08fb20df5dc5e554fcdf6645df9e4
joaovirone <jovmamikl@gmail.com>
2026-01-19 20:53:17 -0300

    Initial commit for repository

---
+++ phone_register/src/main/java/com/phone_register/phone_register/PhoneRegisterApplication.java
@@ -0,0 +1,13 @@
+package com.phone_register.phone_register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneRegisterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoneRegisterApplication.class, args);
+	}
+
+}
